a712dd9b0b8f0023c0a8946a618b6db75001ae24
juaneugenio <leuviah77@gmail.com>
2025-01-17 19:16:04 +0100

    improving fetching in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import PropTypes from "prop-types";
-import data from "../public/recipeData.json"
 import { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ItemList from "./components/ItemList"
@@ -13,22 +12,28 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   
-  useEffect(() => {
-    try {
-			// Converting recipe.id to Number
+useEffect(() => {
+	fetch("/favorite-recipes/recipeData.json")
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((data) => {
 			const formattedData = data.map((recipe) => ({
 				...recipe,
 				id: Number(recipe.id),
 			}));
 			setItems(formattedData);
-      setIsLoading(false)
-		} catch (err) {
-      console.log('👉 Line-24 ▶︎▶︎', err.message);
-      setError("Failed to load data.")
-      setIsLoading(false)
-    }
-    
-	}, []);
+			setIsLoading(false);
+		})
+		.catch((err) => {
+			console.log("👉 Line-24 ▶︎▶︎", err.message);
+			setError("Failed to load data.");
+			setIsLoading(false);
+		});
+}, []);
 
   const updateFavoriteStatus = (id, isFavorite) => {
 		setItems((prevItems) => prevItems.map((item) => (item.id === id ? { ...item, isFavorite } : item)));
